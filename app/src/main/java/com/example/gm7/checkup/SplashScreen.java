03dbb887bbe239a49498e690b28a110ca478e7fd
Elgeneral <magdyelsayed47@gmail.com>
2016-07-24 15:13:31 +0200

    update_database

--- app/src/main/java/com/example/gm7/checkup/SplashScreen.java
+++ app/src/main/java/com/example/gm7/checkup/SplashScreen.java
@@ -6,26 +6,16 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.askerlap.emadahmed.checkup.R;
-import com.twitter.sdk.android.Twitter;
-import com.twitter.sdk.android.core.TwitterAuthConfig;
-import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by PEACE on 3/30/2016.
  */
 public class SplashScreen extends AppCompatActivity {
 
-    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
-    private static final String TWITTER_KEY = "p1fKwcpujXWoSQwRtM1QknpVJ";
-    private static final String TWITTER_SECRET = "AygoLtkDWxKu07NK8IO2ZMy2etvRffEa2idsYhBbUTlU3s9eD6";
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
-        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
-        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.splash);
         assert getSupportActionBar() !=null;
         getSupportActionBar().hide();
