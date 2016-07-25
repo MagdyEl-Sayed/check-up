1ac9919e92c4eedc69844e46d06ebebd1bbd3b9b
emad3020 <askerasker2016@gmail.com>
2016-07-25 23:48:06 +0200

    Sales Items layout created

--- app/src/main/java/com/example/gm7/checkup/sales_items.java
+++ app/src/main/java/com/example/gm7/checkup/sales_items.java
@@ -24,6 +24,7 @@ public class sales_items extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                finish();
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
