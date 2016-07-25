c0f698ede4576b07303725923d5da8c3dfec5742
emad3020 <askerasker2016@gmail.com>
2016-07-25 16:25:20 +0200

    custom listView created

--- app/src/main/java/com/example/gm7/checkup/ListAct.java
+++ app/src/main/java/com/example/gm7/checkup/ListAct.java
@@ -6,44 +6,60 @@ package com.example.gm7.checkup;
 
 
 import android.content.Context;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.askerlap.emadahmed.checkup.R;
 
-public class ListAct extends ArrayAdapter<String> {
+public class ListAct extends BaseAdapter{
     private final Context context;
     private final String[] values;
 
     public ListAct(Context context, String[] values) {
-        super(context, R.layout.activity_shops, values);
+
         this.context = context;
         this.values = values;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        TextView name,address,phone,lastVisit,bill;
         LayoutInflater inflater = (LayoutInflater) context
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-        View rowView = inflater.inflate(R.layout.activity_shops, parent, false);
-        TextView textView = (TextView) rowView.findViewById(R.id.label);
-        ImageView imageView = (ImageView) rowView.findViewById(R.id.logo);
-        textView.setText(values[position]);
-
-        // Change icon based on name
-        String s = values[position];
-
-        System.out.println(s);
-
-        if (s.equals("item1")) {
-            imageView.setImageResource(R.drawable.back_app);
-        }
+        View rowView = inflater.inflate(R.layout.custom_listview, parent, false);
+        name=(TextView)rowView.findViewById(R.id.tv_shopName);
+        address=(TextView)rowView.findViewById(R.id.tv_shopAddress);
+        phone=(TextView)rowView.findViewById(R.id.tv_ShopPhone);
+        lastVisit=(TextView)rowView.findViewById(R.id.tv_lastVisit);
+        bill=(TextView)rowView.findViewById(R.id.tv_totalBill);
+        name.setText(R.string.shopName);
+        phone.setText(R.string.shopPhone);
+        address.setText(R.string.shopAddress);
+        lastVisit.setText(R.string.lastVisit);
+        bill.setText(R.string.totalBill);
 
         return rowView;
+    }
+
+    @Override
+    public int getCount() {
+        return values.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return i;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
     }
 }
--- app/src/main/java/com/example/gm7/checkup/Shops.java
+++ app/src/main/java/com/example/gm7/checkup/Shops.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Context;
 import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -19,25 +20,23 @@ import com.askerlap.emadahmed.checkup.R;
 
 import java.util.ArrayList;
 
-public class Shops extends ListActivity {
-
+public class Shops extends AppCompatActivity {
+private ListView list_layout;
     static final String[] Android =
             new String[] { "item1", "item2", "item3", "item4"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shops);
       //  setContentView(R.layout.activity_shops);
-        setListAdapter(new ListAct(this, Android));
+        list_layout=(ListView)findViewById(R.id.activity_shops);
+        list_layout.setAdapter(new ListAct(this,Android));
+
+//        setListAdapter(new ListAct(this, Android));
 
 
     }
-    @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
 
-        //get selected items
-        String selectedValue = (String) getListAdapter().getItem(position);
-        Toast.makeText(this, selectedValue, Toast.LENGTH_SHORT).show();
-    }
 
 
 }
