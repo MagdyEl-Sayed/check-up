70ee3a4e824026e3b052348bfcdfc819c299fecf
Elgeneral <magdyelsayed47@gmail.com>
2016-07-31 01:06:03 +0200

    slid_show

--- app/src/main/java/com/example/gm7/checkup/MainActivity.java
+++ app/src/main/java/com/example/gm7/checkup/MainActivity.java
@@ -1,32 +1,40 @@
 package com.example.gm7.checkup;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ViewFlipper;
 
 import com.askerlap.emadahmed.checkup.R;
-
-
 public class MainActivity extends AppCompatActivity {
 int counter=0;
+    Animation fade_in, fade_out;
+    ViewFlipper viewFlipper;
     private Button shopping,sales_items;
+
     //Fuck u _!_
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_man_menu   );
+        viewFlipper = (ViewFlipper) this.findViewById(R.id.bckgrndViewFlipper1);
+        fade_in = AnimationUtils.loadAnimation(this,
+                android.R.anim.fade_in);
+        fade_out = AnimationUtils.loadAnimation(this,
+                android.R.anim.fade_out);
+        viewFlipper.setInAnimation(fade_in);
+        viewFlipper.setOutAnimation(fade_out);
+//sets auto flipping
+        viewFlipper.setAutoStart(true);
+        viewFlipper.setFlipInterval(1500);
+        viewFlipper.startFlipping();
         shopping=(Button) findViewById(R.id.btn_shopping);
         sales_items=(Button)findViewById(R.id.btn_salesItem);
         sales_items.setOnClickListener(new View.OnClickListener() {
